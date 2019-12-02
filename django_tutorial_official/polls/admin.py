9bc6d9b61aa6a4c6d0a805d47bc315052b1fe941
Madyan <madyaneka@gmail.com>
2019-12-02 16:38:05 +0700

    kustomisasi form laman admin

--- django_tutorial_official/polls/admin.py
+++ django_tutorial_official/polls/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),  # pylint: disable=C0326
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
